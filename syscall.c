ab8bbf55ae7bdfb358929835e65bf122f47d27f7
jennifershinshin <jennifershinshin@gmail.com>
2018-03-07 14:09:04 -0800

    updated changes. we understand now

--- syscall.c
+++ syscall.c
@@ -19,8 +19,8 @@ fetchint(uint addr, int *ip)
 {
   struct proc *curproc = myproc();
 
-  if(addr >= (KERNBASE + 4) || addr+4 > (curproc->sz + PGSIZE - 1))
-    return -1;
+//  if(addr >= (KERNBASE + 4) || addr+4 > (curproc->sz + PGSIZE - 1))
+//    return -1;
   *ip = *(int*)(addr);
   return 0;
 }
@@ -34,8 +34,8 @@ fetchstr(uint addr, char **pp)
   char *s, *ep;
   struct proc *curproc = myproc();
 
-  if(addr >= (KERNBASE + 4))
-    return -1;
+//  if(addr >= (KERNBASE + 4))
+//    return -1;
   *pp = (char*)addr;
   ep = (char*)curproc->sz;
   for(s = *pp; s < ep; s++){
@@ -63,8 +63,8 @@ argptr(int n, char **pp, int size)
  
   if(argint(n, &i) < 0)
     return -1;
-  if(size < 0 || (uint)i >= (KERNBASE + 4) || (uint)i+size > (curproc->sz + PGSIZE - 1))
-    return -1;
+//  if(size < 0 || (uint)i >= (KERNBASE + 4) || (uint)i+size > (curproc->sz + PGSIZE - 1))
+//    return -1;
   *pp = (char*)i;
   return 0;
 }
