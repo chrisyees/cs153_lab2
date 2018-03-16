af62bd9886e08bdf25debf4c32b1ff28e4c9e92d
jennifershinshin <jennifershinshin@gmail.com>
2018-03-15 22:59:32 -0700

    save this shm.c baby

--- shm.c
+++ shm.c
@@ -29,26 +29,38 @@ void shminit() {
 }
 
 int shm_open(int id, char **pointer) {
-	int x;
-//you write this
-	initlock(&(shm_table.lock), "SHM lock");
 	acquire(&(shm_table.lock));
-	//Case 1
-	for(i = 0; i < 64; i+=)
+	for(int i = 0; i < 64; i++)
 	{
 		if(shm_table.shm_pages[i].id == id)
 		{
-			x = mappages(myproc()->PGDIR, PGROUNDUP(myproc()->sz, V2P(shm_table.shm_pages[i].frame, PTE_W|PTE_U);
-		return (char *)x;	
+			mappages(myproc()->pgdir, PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+			shm_table.shm_pages[i].refcnt++;
+			*pointer = (char *)PGROUNDUP(myproc()->sz);
+			myproc()->sz++;
+			release(&(shm_table.lock));
+			return 0; //0 means it found id we're opening already exists
 		}
 	}
-	//Case 2
 	release(&(shm_table.lock));
-	
 
+	//didn't find shared memory segment. first to allocate
+	acquire(&(shm_table.lock));
+	for(int i = 0; i < 64; i++)
+	{
+		if(shm_table.shm_pages[i].refcnt == 0)
+		{
+			shm_table.shm_pages[i].id = id;
+			shm_pages[i].frame = kalloc();
+			shm_table.shm_pages[i].refcnt = 1;
+			mappages(myproc()->pgdir, PGROUNDUP(myproc()->sz), PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+			*pointer = (char *)PGROUNDUP(myproc()->sz);
+			myproc()->sz++;	
+		}
+	}
+	release(&(shm_table.lock));
 
-
-return 0; //added to remove compiler warning -- you should decide what to return
+	return 1; //I made 1 mean it didnt find shared memory segment so it's first to allocate
 }
 
 
